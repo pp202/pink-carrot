fb9f8d30a56034340f106dcd2b1764d2c69df36d
Pavel <pavel.plotnic@komgo.io>
2026-02-22 21:29:00 -0800

    Upgrade Next.js from 13 to 16

--- src/app/api/lists/[id]/route.ts
+++ src/app/api/lists/[id]/route.ts
@@ -1,12 +1,16 @@
 import { deleteList } from "@/backend/lists";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(request: NextRequest , { params }: { params: { id: string } } ) {
-    const deleted = await deleteList(parseInt(params.id));
-    if (deleted.count === 0) {
-        return NextResponse.json({ message: 'List not found' }, { status: 404 });
-    }
-
-    return NextResponse.json({ message: 'List deleted' }, { status: 200 });
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const deleted = await deleteList(parseInt(id));
+  if (deleted.count === 0) {
+    return NextResponse.json({ message: "List not found" }, { status: 404 });
   }
+
+  return NextResponse.json({ message: "List deleted" }, { status: 200 });
+}
   
