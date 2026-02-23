89083790e3c98d5d8652d7bb1fe1b0317f89806a
Pavel <pavel.plotnic@komgo.io>
2026-02-23 09:03:58 -0800

    Next-auth provider configuration update

--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
 import { createUserIfNew, getUser } from "@/backend/user";
 import NextAuth from "next-auth";
 import type { NextAuthConfig } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import Google from "next-auth/providers/google"
 
 export const authConfig = {
   pages: {
@@ -11,12 +11,8 @@ export const authConfig = {
     strategy: "jwt",
   },
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
+    Google,
   ],
-  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async signIn({ user }) {
       if (user.email) {
